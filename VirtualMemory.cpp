bf2931b8801cba6a58d5e5276aa383e2e97c5428
owner <eran.chashmonay@mail.huji.ac.il>
2020-06-17 20:01:44 +0300

    beginning

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+#define OFFSET_MAX 15
+
+void clearTable(uint64_t frameIndex);
+
 struct PageTable
 {
     uint64_t baseAddr;
@@ -11,23 +15,42 @@ struct PageTable
 
 uint64_t findPageNumber(uint64_t address, uint64_t depth)
 {
-    uint64_t pageNumber = address >> (TABLES_DEPTH - depth);
+    uint64_t pageNumber = address >> (TABLES_DEPTH - depth); //TODO verify calculations
     return pageNumber;
 }
+
 
 word_t findAddress(uint64_t virtualAddr)
 {
     word_t tempWord = 0;
+    uint64_t victim;
     uint64_t tempAddress = findPageNumber(virtualAddr, 1);
     PMread(0 + tempAddress, &tempWord);
+
+    if (tempWord == 0)
+    {
+        victim = findNextFrame();
+        clearTable(victim);
+        PMwrite(0 + tempAddress, victim);
+        tempWord = victim;
+    }
+
    for (uint64_t depth = 2; depth < TABLES_DEPTH; depth++)
    {
        tempAddress = findPageNumber(tempAddress, depth);
-       if (tempAddress == 0)
+       PMread(tempWord*PAGE_SIZE + tempAddress, &tempWord);
+       if (tempWord == 0)
        {
+            victim = findNextFrame();
+           clearTable(victim);
+           if (tempAddress <= 16)
+           {
+               PMrestore(victim, virtualAddr);
+           }
+           PMwrite(tempWord*PAGE_SIZE + tempAddress, victim);
 //           TODO evict n'stuff
        }
-       PMread(tempWord*PAGE_SIZE + tempAddress, &tempWord);
+
    }
    return tempWord;
 }
