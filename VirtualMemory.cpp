fa53b4a7a8974a5e628f4c211bf1d889a3ed1169
owner <eran.chashmonay@mail.huji.ac.il>
2020-06-20 12:17:29 +0300

    added cyclic distance function

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,12 +1,22 @@
 #include "VirtualMemory.h"
 #include "PhysicalMemory.h"
 
-#include <stdio.h>
+#include <cstdio>
 
 struct Frame
 {
     word_t frameIndex;
     word_t pageIndex;
+};
+
+struct DFSResult
+{
+    bool empty;
+    word_t maxDepth;
+    word_t deepestFrameIndex;
+    word_t deepestPageIndex;
+    word_t maxFrameIndex;
+    word_t pathToDeepest;
 };
 
 void clearTable(uint64_t frameIndex);
@@ -24,47 +34,118 @@ bool isFrameEmpty(uint64_t frameIndex)
     return true;
 }
 
-
-Frame findFrameToEvict()
+DFSResult DFSRecursive(word_t parent, word_t curFrame, word_t depth, word_t pathToCurrent)
 {
-    int maxDepth = 0;
-    int curDepth = 0;
-    Frame farestFrame = Frame();
-    farestFrame.frameIndex = 0;
-    farestFrame.pageIndex = 0;
+    DFSResult result({false,0,0,0, 0, 0});
 
-}
-
-
-word_t findNextFrameIndex()
-{
-    for (auto frame = 1; frame < NUM_FRAMES; ++frame)
+    if (curFrame == parent)
     {
-        if(isFrameEmpty(frame))
+        return result;
+    }
+
+    word_t tempWord;
+    //base case 1
+    if (isFrameEmpty(curFrame))
+    {
+        result.empty = true;
+        result.maxDepth = depth;
+        result.maxFrameIndex = curFrame;
+        result.deepestFrameIndex = curFrame;
+        return result;
+    }
+    //base case 2
+    if (depth >= TABLES_DEPTH)
+    {
+        result.maxDepth = depth;
+        result.maxFrameIndex = curFrame;
+        result.deepestFrameIndex = curFrame;
+        return result;
+    }
+
+    for (word_t frame = 0; frame < PAGE_SIZE; frame++)
+    {
+        uint64_t pathToNext = ((pathToCurrent << OFFSET_WIDTH) | (frame));
+        PMread(curFrame*PAGE_SIZE + frame, &tempWord);
+        if (tempWord != 0)
         {
-            return frame;
+            DFSResult curResult = DFSRecursive(parent, tempWord, depth + 1, pathToNext);
+            if (curResult.empty && !result.empty)
+            {
+                result.maxFrameIndex = curResult.maxFrameIndex;
+                result.empty = true;
+                result.deepestFrameIndex = curResult.deepestFrameIndex;
+                result.maxDepth = curResult.maxDepth;
+            }
+            else if ((curResult.empty && result.empty) || (!curResult.empty && !result.empty))
+            {
+                if (curResult.maxFrameIndex > result.maxFrameIndex)
+                {
+                    result.maxFrameIndex = curResult.maxFrameIndex;
+                }
+                if (curResult.maxDepth > result.maxDepth)
+                {
+                    result.maxDepth = curResult.maxDepth;
+                    result.deepestFrameIndex = curResult.deepestFrameIndex;
+                    result.deepestPageIndex = pathToNext;
+                }
+            }
         }
     }
+    return result;
+}
+
+word_t findCyclicDistance(uint64_t from, uint64_t to)
+{
+    if (from - to > NUM_PAGES - (from - to))
+        return from - to;
+    return NUM_PAGES - (from - to);
+}
+
+word_t findNextFrameIndex(word_t parent)
+{
+    DFSResult result = DFSRecursive(parent, 0, 1);
+    if (result.empty)
+    {
+        return result.deepestFrameIndex;
+    }
+    if (result.maxFrameIndex + 1 < NUM_FRAMES)
+    {
+        return result.maxFrameIndex + 1;
+    }
+
+    PMevict();
+
+//    for (auto frame = 1; frame < NUM_FRAMES; ++frame)
+//    {
+//        if(isFrameEmpty(frame) && frame != parent)
+//        {
+//            return frame;
+//        }
+//    }
+
     // TODO need to find a page to evict
     // finding the deepest frame index and page index of this frame (virtual memory address)
     word_t index = 1; //just numbers to try for now
     uint64_t pageIndex = 5;
     PMevict(index, pageIndex);
     return index;
-
 }
 
 uint64_t findPageNumber(uint64_t address, uint64_t depth)
 {
     //uint64_t pageNumber = address >> (TABLES_DEPTH - depth); //TODO verify calculations
 
-    uint64_t offsetMask = 0;
-    for (uint64_t i = 0; i < OFFSET_WIDTH; i++)
-    {
-        offsetMask += 1LL << i;
-    }
+
     uint64_t pageNumber = (address >> ((TABLES_DEPTH - (depth + 1)) * OFFSET_WIDTH));
-    pageNumber = pageNumber & (offsetMask);
+    if (depth > 1)
+    {
+        uint64_t offsetMask = 0;
+        for (uint64_t i = 0; i < OFFSET_WIDTH; i++)
+        {
+            offsetMask += 1LL << i;
+        }
+        pageNumber = pageNumber & (offsetMask);
+    }
     return pageNumber;
 }
 
@@ -73,9 +154,9 @@ word_t findAddress(uint64_t virtualAddr)
 {
     word_t tempWord = 0;
     word_t victim;
+    word_t lastPage = 0;
     uint64_t curPageNumber = findPageNumber(virtualAddr, 1);      // Top level table
     PMread(0 + curPageNumber, &tempWord);
-
     if (tempWord == 0)
     {
         victim = findNextFrameIndex();
@@ -83,6 +164,7 @@ word_t findAddress(uint64_t virtualAddr)
         PMwrite(0 + curPageNumber, victim);
         tempWord = victim;
     }
+    lastPage = word_t(tempWord);
 
    for (uint64_t depth = 2; depth < TABLES_DEPTH; depth++)
    {
@@ -91,15 +173,21 @@ word_t findAddress(uint64_t virtualAddr)
        if (tempWord == 0)
        {
            victim = findNextFrameIndex();
+           if (victim == lastPage)
+           {
+               //TODO pick second last
+           }
+           //           TODO evict n'stuff
            clearTable(victim);
-           if (curPageNumber < 16)     //
+           if (curPageNumber < 16)     // only if actual page
            {
                PMrestore(victim, virtualAddr);
            }
            PMwrite(tempWord*PAGE_SIZE + curPageNumber, victim);
-//           TODO evict n'stuff
-       }
+           tempWord = victim;
 
+       }
+       lastPage = word_t(tempWord);
    }
    return tempWord;
 }
